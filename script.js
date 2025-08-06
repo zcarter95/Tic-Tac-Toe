4195eecb8f9d58565145310b463492de5ff5410c
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-08-06 10:40:16 -0700

    progress on game manager

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gameboard = (function() {
     for (let x = 0; x < rows; x++) {
         board[x] = [];
         for (let y = 0; y < columns; y++) {
-            board[x].push(`${x},${y}`);
+            board[x].push(" - ");
         }
     }
     const getGameBoard = () => {
@@ -18,10 +18,17 @@ const gameboard = (function() {
         console.log(board[2]);
     }
     const play = (player, row,column) => {
-        board[row][column] = player.symbol
+        if (board[row][column] === " - "){
+            board[row][column] = ` ${player.symbol} `
+        }
+        else {
+            console.log(`(${row},${column}) is occupied!`)
+            return "error"
+        }
+        printBoard();
     }
 
-    return {getGameBoard, play, printBoard}
+    return {getGameBoard, play}
 })();
 
 const createPlayer = (name, symbol) => {
@@ -34,7 +41,20 @@ const gameManager = (() => {
     const start = (name1, name2) => {
         player1 = createPlayer(name1, "X");
         player2 = createPlayer(name2, "O");
+        console.log(`Player1: ${player1.name} is playing ${player1.symbol}`)
+        console.log(`Player2: ${player2.name} is playing ${player2.symbol}`)
     }
-    return { start }
-
-});
+    let playCounter = 0;
+    const play = (x, y) => {
+        playCounter++;
+        if (playCounter % 2 !== 0) {
+            let play = gameboard.play(player1, x, y);
+            if (play == "error") {playCounter--}
+        }
+        else {
+            let play = gameboard.play(player2, x, y);
+            if (play == "error") {playCounter--}
+        }
+    }
+    return { start, play }
+})();
