f14175b6a45bc1c52fcde2f561094de12e8380a3
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-08-06 09:14:20 -0700

    gameboard playing pieces and printing works

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ const gameboard = (function() {
     const columns = 3;
     const board = [];
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let x = 0; x < columns; x++) {
-            board[i].push(0);
+    for (let x = 0; x < rows; x++) {
+        board[x] = [];
+        for (let y = 0; y < columns; y++) {
+            board[x].push(`${x},${y}`);
         }
     }
     const getGameBoard = () => {
         return board;
     }
-    return {getGameBoard}
+    const printBoard = () => {
+        console.log(board[0]);
+        console.log(board[1]);
+        console.log(board[2]);
+    }
+    const play = (player, row,column) => {
+        board[row][column] = player.symbol
+    }
+
+    return {getGameBoard, play, printBoard}
 })();
 
-const createPlayer = (name) => {
-    return {player}
+const createPlayer = (name, symbol) => {
+    return { name, symbol }
 }
 
-const gameController = (player1 = "Player 1", player2 = "Player 2") => {
-    const 
-}
+const gameManager = (() => {
+    let player1 = null;
+    let player2 = null;
+    const start = (name1, name2) => {
+        player1 = createPlayer(name1, "X");
+        player2 = createPlayer(name2, "O");
+    }
+    return { start }
+
+});
